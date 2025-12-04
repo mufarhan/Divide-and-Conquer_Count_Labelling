ed6b3a82ea2742bb75d9cd21124749e51b137fb9
Muhammad Farhan <mufarhan.pervaiz@gmail.com>
2025-12-04 21:12:34 +1100

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,13 @@
+# Prepare dataset and queries
+make
+bash download.sh
+bash generate_query.sh
+
+# DCL
+bash index_dcl.sh
+bash query_dcl.sh
+
+# TC
+bash install_tc.sh
+bash index_tc.sh
+bash query_tc.sh
